9e3d7efbe1ea4869f3718c261b3ba1d517aa24e6
Dclasyc <107035049+Dclasyc@users.noreply.github.com>
2022-07-20 05:04:31 +0100

    Add files via upload

--- tailorshub.php
+++ tailorshub.php
@@ -1,76 +1,10 @@
-
 <?php 
-	
-	ob_start();
-	session_start();
+
+	include_once "portal_navigation.php";
 
  ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-
-	<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"> -->
-
-	<!-- <link rel="stylesheet" type="text/css" href="finalproject.css"> -->
-
-	<style>
-  <?php include "finalproject.css" ?>
-</style>
-
-<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
-
-	<title>Tailors Hub</title>
-</head>
-<body>
-
-
-			<!-- Navigation panel -->
-			
-
-<nav class="navbar sticky-top navbar-expand-lg navbar-light" id="navdiv">
-  <div class="container-fluid">
-
-    <a class="" href="index.php">
-		  <img src="images/navlogoedit.png" alt="logo" height="80" style="border-radius: 50%;">
-	 </a>
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #ff950a;">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0" id="navigation_item">
-         
-		        <li class="nav-item">
-		          <a class="nav-link navtext" href="login.php">Log In</a>
-		        </li>
-
-		        <li class="nav-link navtext" id="login_signup_divider">|</li>
-
-		        <li class="nav-item">
-		          <a class="nav-link navtext" href="customersignup.php">Customer Sign Up</a>
-		        </li>
-
-		        <li class="nav-link navtext" id="login_signup_divider">|</li>
-
-		        <li class="nav-item">
-		          <a class="nav-link navtext" href="tailorsignup.php">Tailor Sign Up</a>
-		        </li>
-      </ul>
-      
-        <a href="#"><button class="btn" id="btnsell" type="button">SELL PRODUCTS</button></a>
-      
-    </div>
-  </div>
-</nav>
-
-
-		<!-- BODY OF WEBPAGE -->
+<!-- BODY OF WEBPAGE -->
 
 <div class="container-fluid" id="parentdiv" style="background-color: rgba(218, 227, 231, 0.4);">	
 
@@ -116,6 +50,7 @@
 	</div>
 
 
+	
 	<div class="row mb-3" id="sidepanel_and_landingpics_div" style="display: flex; flex-wrap: nowrap;">
 		
 		<div class="col-lg-4" id="sidepanel">
@@ -213,23 +148,15 @@
 
 
 
+
 	<!-- FOOTER SECTION -->
 
-	<div class="container-fluid" id=footerdiv>
+	<?php 
 
-	<div class="">
-		
-	</div>
+		include_once "footer.php"
 
-	<div id="overlay"></div>
-		
-	</div>
+	 ?>
 
 
 </body>
 </html>
-<?php 
-
-	ob_end_flush();
-
- ?>
